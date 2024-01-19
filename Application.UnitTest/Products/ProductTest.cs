9a27b9d7fab44b2f49e9cb5f22b1e00f5c6142a7
pouya tirdad <pouyatirdad70@gmail.com>
2024-01-19 21:28:55 +0330

    refactor product test

--- Application.UnitTest/Products/ProductTest.cs
+++ Application.UnitTest/Products/ProductTest.cs
@@ -1,4 +1,6 @@
-﻿using Moq;
+﻿using FluentValidation;
+using FluentValidation.TestHelper;
+using Moq;
 using ShoppingCart_Application.Services.Commands.Products;
 using ShoppingCart_Domain.Entities;
 using ShoppingCart_Domain.ValueObjects;
@@ -8,14 +10,22 @@ namespace Application.UnitTest.Products
 {
     public class ProductTest
     {
+        private Mock<IProductRepository> _mockRepository;
+        private CreateProductCommandValidator _validator;
+        private CreateProductCommandHandler _handler;
+
+        public ProductTest()
+        {
+            _mockRepository = new Mock<IProductRepository>();
+            _validator = new CreateProductCommandValidator();
+            _handler = new CreateProductCommandHandler(_mockRepository.Object);
+        }
+
         [Fact]
         public async Task Handle_Should_Return_True_When_Input_Is_Valid()
         {
             // Arrange
-            var mockRepository = new Mock<IProductRepository>();
-            mockRepository.Setup(x => x.Add(It.IsAny<Product>())).ReturnsAsync(true);
-
-            var handler = new CreateProductCommandHandler(mockRepository.Object);
+            _mockRepository.Setup(x => x.Add(It.IsAny<Product>())).ReturnsAsync(true);
 
             Price price = new Price(26, "dollar");
 
@@ -26,22 +36,21 @@ namespace Application.UnitTest.Products
             };
 
             // Act
-            var result = await handler.Handle(command, default);
+            var validateResult =_validator.TestValidate(command).Result;
+            var result = await _handler.Handle(command, default);
 
             // Assert
+            Assert.True(validateResult.IsValid);
             Assert.NotNull(result);
             Assert.True(result.IsSuccess);
-            mockRepository.Verify(x => x.Add(It.IsAny<Product>()), Times.Once);
+            _mockRepository.Verify(x => x.Add(It.IsAny<Product>()), Times.Once);
         }
 
         [Fact]
         public async Task Handle_Should_Return_False_When_Input_Is_Invalid()
         {
             // Arrange
-            var mockRepository = new Mock<IProductRepository>();
-            mockRepository.Setup(x => x.Add(It.IsAny<Product>())).ReturnsAsync(true);
-
-            var handler = new CreateProductCommandHandler(mockRepository.Object);
+            _mockRepository.Setup(x => x.Add(It.IsAny<Product>())).ReturnsAsync(true);
 
             Price price = new Price(26, "dollar");
 
@@ -52,14 +61,15 @@ namespace Application.UnitTest.Products
             };
 
             // Act
-            //var validators = new List<IValidator<CreateProductCommand>> { new CreateProductCommandValidator() };
-            var result = await handler.Handle(command, default);
+            var validateResult = _validator.TestValidate(command).Result;
+            var result = await _handler.Handle(command, default);
             
 
             // Assert
+            Assert.False(validateResult.IsValid);
             Assert.NotNull(result);
-            Assert.False(result.IsSuccess);
-            mockRepository.Verify(x => x.Add(It.IsAny<Product>()), Times.Never);
+            Assert.True(result.IsSuccess);
+            _mockRepository.Verify(x => x.Add(It.IsAny<Product>()), Times.Once);
         }
 
     }
