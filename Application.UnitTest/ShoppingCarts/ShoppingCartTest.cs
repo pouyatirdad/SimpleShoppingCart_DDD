9e6191ff00a50b3ccf4a0d80262e274423b471a0
pouya tirdad <pouyatirdad70@gmail.com>
2024-01-30 21:56:17 +0330

    complete addproducttoshoppingcart test

--- Application.UnitTest/ShoppingCarts/ShoppingCartTest.cs
+++ Application.UnitTest/ShoppingCarts/ShoppingCartTest.cs
@@ -10,31 +10,68 @@ namespace Application.UnitTest.ShoppingCarts
 {
     public class ShoppingCartTest
     {
-        private Mock<IShoppingCartRepository> _mockRepository;
-        private CreateShoppingCartCommandHandler _handler;
-
-        public ShoppingCartTest()
-        {
-            _mockRepository = new Mock<IShoppingCartRepository>();
-            _handler = new CreateShoppingCartCommandHandler(_mockRepository.Object);
-        }
         #region CreateShoppingCart
         [Fact]
         public async Task CreateShoppingCart_Should_Return_True_When_Input_Is_Valid()
         {
             // Arrange
-            _mockRepository.Setup(x => x.Add(It.IsAny<ShoppingCart>())).ReturnsAsync(true);
+            var shoppingCartMockRepository = new Mock<IShoppingCartRepository>();
+            shoppingCartMockRepository.Setup(x => x.Add(It.IsAny<ShoppingCart>())).ReturnsAsync(true);
 
             var command = new CreateShoppingCartCommand();
+            var handler = new CreateShoppingCartCommandHandler(shoppingCartMockRepository.Object);
 
             // Act
-            var result = await _handler.Handle(command, default);
+            var result = await handler.Handle(command, default);
 
             // Assert
             Assert.NotNull(result);
             Assert.True(result.IsSuccess);
             Assert.True(Guid.TryParse(result.Data.ToString(), out _));
-            _mockRepository.Verify(x => x.Add(It.IsAny<ShoppingCart>()), Times.Once);
+            shoppingCartMockRepository.Verify(x => x.Add(It.IsAny<ShoppingCart>()), Times.Once);
+        }
+        #endregion
+        #region AddProductToShoppingCart
+        [Fact]
+        public async Task AddProductToShoppingCart_Should_Return_True_When_Input_Is_Valid()
+        {
+            // Arrange
+            var shoppingCartMockRepository = new Mock<IShoppingCartRepository>();
+            var productMockRepository = new Mock<IProductRepository>();
+
+            var firstPrice =new Price(1000,"dollar");
+            var secondPrice =new Price(1200, "dollar");
+
+            var firstProduct =new Product(Guid.NewGuid(),"first",firstPrice);
+            var secondProduct =new Product(Guid.NewGuid(),"second",secondPrice);
+
+            var command = new AddProductToShoppingCartCommand
+            {
+                ProductsId = new List<Guid>() { firstProduct.Id,secondProduct.Id },
+                ShoppingCartId = Guid.NewGuid()
+            };
+
+            var shoppingCart =new ShoppingCart(command.ShoppingCartId);
+            shoppingCartMockRepository.Setup(x => x.Get(command.ShoppingCartId)).ReturnsAsync(shoppingCart);
+            shoppingCartMockRepository.Setup(x=>x.Update(It.IsAny<ShoppingCart>())).ReturnsAsync(true);
+
+            productMockRepository.Setup(x => x.Get(firstProduct.Id)).ReturnsAsync(firstProduct);
+            productMockRepository.Setup(x => x.Get(secondProduct.Id)).ReturnsAsync(secondProduct);
+
+            var handler = new AddProductToShoppingCartCommandHandler(productMockRepository.Object, shoppingCartMockRepository.Object);
+
+            // Act
+            var result = await handler.Handle(command, default);
+
+            // Assert
+            Assert.True(result.IsSuccess);
+            Assert.NotNull(result.Data);
+
+            Assert.Equal(2,result.Data.Items.Count);
+            var actualTotal = result.Data.Total.Amount;
+            var expectedTotal = result.Data.Items[0].Price.Amount + result.Data.Items[1].Price.Amount;
+            Assert.Equal(expectedTotal, actualTotal);
+            Assert.Equal(result.Data.Id,command.ShoppingCartId);
         }
         #endregion
     }
