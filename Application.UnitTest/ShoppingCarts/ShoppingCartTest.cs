0cd1da02a9898bb973acf8b60281c982d18a7bc4
pouya tirdad <pouyatirdad70@gmail.com>
2024-02-02 22:37:46 +0330

    write test fot GetShoppingCartsQuery

--- Application.UnitTest/ShoppingCarts/ShoppingCartTest.cs
+++ Application.UnitTest/ShoppingCarts/ShoppingCartTest.cs
@@ -1,10 +1,12 @@
 ﻿using Moq;
 using ShoppingCart_Application.Services.Commands.Products;
 using ShoppingCart_Application.Services.Commands.ShoppingCarts;
+using ShoppingCart_Application.Services.Queries.ShoppingCarts;
 using ShoppingCart_Domain.Entities;
 using ShoppingCart_Domain.ValueObjects;
 using ShoppingCart_infrastructure.Repositories;
 using System.Diagnostics;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Application.UnitTest.ShoppingCarts
 {
@@ -39,21 +41,21 @@ namespace Application.UnitTest.ShoppingCarts
             var shoppingCartMockRepository = new Mock<IShoppingCartRepository>();
             var productMockRepository = new Mock<IProductRepository>();
 
-            var firstPrice =new Price(1000,"dollar");
-            var secondPrice =new Price(1200, "dollar");
+            var firstPrice = new Price(1000, "dollar");
+            var secondPrice = new Price(1200, "dollar");
 
-            var firstProduct =new Product(Guid.NewGuid(),"first",firstPrice);
-            var secondProduct =new Product(Guid.NewGuid(),"second",secondPrice);
+            var firstProduct = new Product(Guid.NewGuid(), "first", firstPrice);
+            var secondProduct = new Product(Guid.NewGuid(), "second", secondPrice);
 
             var command = new AddProductToShoppingCartCommand
             {
-                ProductsId = new List<Guid>() { firstProduct.Id,secondProduct.Id },
+                ProductsId = new List<Guid>() { firstProduct.Id, secondProduct.Id },
                 ShoppingCartId = Guid.NewGuid()
             };
 
-            var shoppingCart =new ShoppingCart(command.ShoppingCartId);
+            var shoppingCart = new ShoppingCart(command.ShoppingCartId);
             shoppingCartMockRepository.Setup(x => x.Get(command.ShoppingCartId)).ReturnsAsync(shoppingCart);
-            shoppingCartMockRepository.Setup(x=>x.Update(It.IsAny<ShoppingCart>())).ReturnsAsync(true);
+            shoppingCartMockRepository.Setup(x => x.Update(It.IsAny<ShoppingCart>())).ReturnsAsync(true);
 
             productMockRepository.Setup(x => x.Get(firstProduct.Id)).ReturnsAsync(firstProduct);
             productMockRepository.Setup(x => x.Get(secondProduct.Id)).ReturnsAsync(secondProduct);
@@ -67,11 +69,53 @@ namespace Application.UnitTest.ShoppingCarts
             Assert.True(result.IsSuccess);
             Assert.NotNull(result.Data);
 
-            Assert.Equal(2,result.Data.Items.Count);
+            Assert.Equal(2, result.Data.Items.Count);
             var actualTotal = result.Data.Total.Amount;
             var expectedTotal = result.Data.Items[0].Price.Amount + result.Data.Items[1].Price.Amount;
             Assert.Equal(expectedTotal, actualTotal);
-            Assert.Equal(result.Data.Id,command.ShoppingCartId);
+            Assert.Equal(result.Data.Id, command.ShoppingCartId);
+        }
+        #endregion
+        #region GetShoppingCarts
+        [Fact]
+        public async Task GetShoppingCarts_Should_Return_True()
+        {
+            // Arrange
+            var shoppingCartMockRepository = new Mock<IShoppingCartRepository>();
+
+            var firstPrice = new Price(1000, "dollar");
+            var secondPrice = new Price(1200, "dollar");
+            var firstProduct = new Product(Guid.NewGuid(), "first", firstPrice);
+            var secondProduct = new Product(Guid.NewGuid(), "second", secondPrice);
+
+            var firstShoppingCart = new ShoppingCart(Guid.NewGuid());
+            firstShoppingCart.AddItem(firstProduct);
+            firstShoppingCart.AddItem(secondProduct);
+
+            var shoppingCarts = new List<ShoppingCart>()
+            {
+                firstShoppingCart
+            };
+
+            shoppingCartMockRepository.Setup(x => x.GetAll()).ReturnsAsync(shoppingCarts);
+
+            var query = new GetShoppingCartsQuery();
+            var handler = new GetShoppingCartsQueryHandler(shoppingCartMockRepository.Object);
+
+            // Act
+            var result = await handler.Handle(query, default);
+
+            // Assert
+            Assert.True(result.IsSuccess);
+            Assert.NotNull(result.Data);
+
+            Assert.Equal(2, result.Data[0].Items.Count);
+            var actualTotal = result.Data[0].Total.Amount;
+            var expectedTotal = result.Data[0].Items[0].Price.Amount + result.Data[0].Items[1].Price.Amount;
+            Assert.Equal(expectedTotal, actualTotal);
+            Assert.Equal(result.Data[0].Id, firstShoppingCart.Id);
+
+            shoppingCartMockRepository.Verify(x => x.GetAll(), Times.Once);
         }
         #endregion
     }
