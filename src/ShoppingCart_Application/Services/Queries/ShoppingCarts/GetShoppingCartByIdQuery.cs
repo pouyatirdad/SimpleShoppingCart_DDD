8bc413551600cc89b3732373fd94d84350e4fafb
pouya tirdad <pouyatirdad70@gmail.com>
2024-02-13 22:44:11 +0330

    add GetShoppingCartByIdQuery

---
+++ src/ShoppingCart_Application/Services/Queries/ShoppingCarts/GetShoppingCartByIdQuery.cs
@@ -0,0 +1,43 @@
+﻿using MediatR;
+using ShoppingCart_Application.Responses;
+using ShoppingCart_Domain.Entities;
+using ShoppingCart_infrastructure.Repositories;
+
+namespace ShoppingCart_Application.Services.Queries.ShoppingCarts
+{
+    #region query
+    public record GetShoppingCartByIdQuery : IRequest<Response<ShoppingCart>>
+    {
+        public Guid ShoppingCartId { get; set; }
+    };
+
+    #endregion
+    #region handler
+    public class GetShoppingCartByIdQueryHandler : IRequestHandler<GetShoppingCartByIdQuery, Response<ShoppingCart>>
+    {
+        private readonly IShoppingCartRepository _shoppingCartRepository;
+
+        public GetShoppingCartByIdQueryHandler(IShoppingCartRepository shoppingCartRepository)
+        {
+            _shoppingCartRepository = shoppingCartRepository;
+        }
+        public async Task<Response<ShoppingCart>> Handle(GetShoppingCartByIdQuery request, CancellationToken cancellationToken)
+        {
+            var response = new Response<ShoppingCart>();
+
+            var data = await _shoppingCartRepository.Get(request.ShoppingCartId);
+
+            if (data != null)
+            {
+                response.Data = data;
+                return response;
+            }
+
+            response.Message = "there is no shoppingCart";
+            response.IsSuccess = false;
+
+            return response;
+        }
+    }
+    #endregion
+}
