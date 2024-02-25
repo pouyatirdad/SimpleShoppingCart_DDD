959bc9562b07b72a6820974a8f4aa994d22c6b1c
pouya tirdad <pouyatirdad70@gmail.com>
2024-02-25 16:46:09 +0330

    add method in productController for get allproducts

--- src/ShoppingCart_API/Controllers/ProductController.cs
+++ src/ShoppingCart_API/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using ShoppingCart_Application.Responses;
 using ShoppingCart_Application.Services.Commands.Products;
+using ShoppingCart_Application.Services.Queries.Products;
 using ShoppingCart_Domain.Entities;
 
 namespace ShoppingCart_API.Controllers
@@ -20,6 +21,13 @@ namespace ShoppingCart_API.Controllers
         {
             var data = await _mediator.Send(product);
 
+            return data;
+        }
+        [HttpGet("GetAll")]
+        public async Task<Response<List<Product>>> GetAllProducts()
+        {
+            var query = new GetProductsQuery();
+            var data = await _mediator.Send(query);
             return data;
         }
     }
